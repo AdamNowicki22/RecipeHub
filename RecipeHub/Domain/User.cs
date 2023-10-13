1e6f0cb0adcf36fa18e91cccca14a32636a2147b
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-10-13 11:18:40 +0200

    cleanup

--- RecipeHub/Domain/User.cs
+++ RecipeHub/Domain/User.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Numerics;
 
 namespace RecipeHub.Domain
 {
